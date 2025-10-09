a54db0de392782ff4ea5c4b2a9135a1549c8f52f
Theofilos Augoustis <theofilos.augoustis@gmail.com>
2025-10-09 09:43:57 +0000

    Update Focaccia plugin to execute every time that a translation executes

--- contrib/plugins/focaccia.c
+++ contrib/plugins/focaccia.c
@@ -11,12 +11,18 @@ static void plugin_exit(qemu_plugin_id_t id, void* p) {
     printf("Plugin has completed!\n");
 }
 
+static void concolic_trace(unsigned int cpu_index, void *udata) {
+    printf("Translation executed on CPU %u\n", cpu_index);
+}
+
+static void register_tracer(qemu_plugin_id_t id, struct qemu_plugin_tb *tb) {
+    qemu_plugin_register_vcpu_tb_exec_cb(tb, concolic_trace,
+                                         QEMU_PLUGIN_CB_R_REGS, NULL);
+}
+
 // argc and argv correspond to the arguments passed via -plugin focaccia.so,arg1=<arg1>,arg2=<arg2>
-QEMU_PLUGIN_EXPORT 
-int qemu_plugin_install(qemu_plugin_id_t id,
-                        const qemu_info_t *info,
-                        int argc, char **argv)
-{
+QEMU_PLUGIN_EXPORT
+int qemu_plugin_install(qemu_plugin_id_t id, const qemu_info_t *info, int argc, char **argv) {
     int i;
 
     // Process plugin arguments
@@ -27,6 +33,7 @@ int qemu_plugin_install(qemu_plugin_id_t id,
 
     plugin_init();
 
+    qemu_plugin_register_vcpu_tb_trans_cb(id, register_tracer);
     qemu_plugin_register_atexit_cb(id, plugin_exit, NULL);
     return 0;
 }
